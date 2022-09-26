7d0d73a8cf95075ae262e0a7309daa6fc811d90e
GemmaThomas-Green <GemmaThomas-Green@DESKTOP-1EOV5JC>
2022-09-26 11:21:54 +0100

    Add project files.

---
+++ Fugitive search/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Fugitive_search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fugitiveLicencePlates = new List<string();
+            string licencePlate;
+            fugitiveLicencePlates.Add("X999GHP");
+            fugitiveLicencePlates.Add("BD51MSR");
+            fugitiveLicencePlates.Add("PR0SPER");
+
+            licencePlate = Console.ReadLine();
+            if (fugitiveLicencePlates.Contains(licencePlate))
+            {
+                Console.WriteLine("This car may be driven by a wanted fugitive");
+            }
+            else
+            {
+                Console.WriteLine("This person is noty suspicious");
+            }
+        }
+    }
+}
